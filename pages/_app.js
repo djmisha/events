7609911e75c9a5013c63df11392b7d2c94bb13ca
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2023-01-30 15:15:37 -0800

    Google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
+import { Poppins } from "@next/font/google";
 import "../styles/global.scss";
 import store from "../features/store";
 import { Provider } from "react-redux";
 
+const poppins = Poppins({
+  weight: ["200", "400", "600"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
 function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <div className={poppins.className}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </div>
   );
 }
 
