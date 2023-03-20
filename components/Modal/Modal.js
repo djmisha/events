d3d4e4d252fa002ab88eea6475aaa26b3b5fadec
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2023-03-20 11:50:23 -0700

    modal back button

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import Image from "next/image";
 
 const Modal = ({ content, title, isOpen, SetIsOpen }) => {
   const handleClose = () => {
     SetIsOpen(false);
   };
+
   return (
     <div className={`modal ${isOpen ? "modal-open" : "modal-closed"}`}>
       <div className="modal-content">
         <div className="modal-close" onClick={handleClose}>
-          ✕
+          <Image
+            width={25}
+            height={25}
+            src="/images/icon-back.svg"
+            alt="Menu"
+          />
+          <span>Back</span>
         </div>
         <h3>{title}</h3>
         <div dangerouslySetInnerHTML={{ __html: content }}></div>
