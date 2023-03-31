0acdea18375640725bda633f875f0f16429ced07
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2023-03-31 02:50:01 -0700

    removes 301 redirects

--- next.config.js
+++ next.config.js
@@ -15,15 +15,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/:slug",
-        destination: "https://music.sandiegohousemusic.com/:slug",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
