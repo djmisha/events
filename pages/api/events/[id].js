3d7999b843ea2dae3a23369bee98e69b40390147
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2023-04-13 23:37:50 -0700

    api endpoint for events

---
+++ pages/api/events/[id].js
@@ -0,0 +1,20 @@
+/**
+ * EDMtrain API
+ * @param {*} req
+ * @param {*} res - event data object
+ */
+
+export default async function handler(req, res) {
+  const { id } = req.query;
+  const KEY = process.env.NEXT_PUBLIC_API_KEY_EDMTRAIN;
+  const EDMURL = process.env.NEXT_PUBLIC_API_URL_EDMTRAIN;
+  const URL = EDMURL + id + "&client=" + KEY;
+
+  try {
+    const apiResponse = await fetch(URL);
+    const data = await apiResponse.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
