d877049e0a1d60390958a35129272614ab84bd64
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2023-07-07 14:07:20 -0700

    endpoint for all events

---
+++ pages/api/allevents.js
@@ -0,0 +1,22 @@
+/**
+ * EDMtrain API
+ * get ALL events - returns a large object (4.2 mb +) so use carefully
+ * @param {*} req
+ * @param {*} res - event data object
+ */
+
+export default async function handler(req, res) {
+  const KEY = process.env.NEXT_PUBLIC_API_KEY_EDMTRAIN;
+  const URL = EDMURL + "&client=" + KEY;
+  const EDMURL = "https://edmtrain.com/api/events?";
+
+  try {
+    const apiResponse = await fetch(URL);
+    const data = await apiResponse.json();
+    res.setHeader("Cache-Control", "s-maxage=21600");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+    console.error(error);
+  }
+}
