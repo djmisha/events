b9f431202cd555752486d0008ee6af5575ecc6c9
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2025-02-14 09:10:17 -0800

    refactor: Replace Navigation component with NavigationBar in cities.js

--- pages/cities.js
+++ pages/cities.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/layout";
 import CitiesStates from "../components/Homepage/CitiesStates";
-import Navigation from "../components/Navigation/Navigation";
 import { getLocations } from "../utils/getLocations";
 import NavigationBar from "../components/NavigationBar/NavigataionBar";
 
