c7b818139b47709aec185b91d78135fa0492a366
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2025-02-22 05:27:39 -0800

    feat: Refactor geolocation logic into a separate service for improved readability and maintainability

--- components/Locator/Locator.js
+++ components/Locator/Locator.js
@@ -1,112 +1,21 @@
 import { useEffect, useRef, useState, useContext } from "react";
 import { AppContext } from "../../features/AppContext.js";
 import { locationUrl } from "../../utils/getLocations";
-import {
-  UserLocationService,
-  hasUserLocation,
-  matchToLocation,
-  matchesCity,
-  getLocationId,
-  createLocationObject,
-} from "../../utils/getUserLocation.js";
-import { urlBigData, cityOrState } from "../../utils/utilities";
+import { cityOrState } from "../../utils/utilities";
 import { getEventsHome } from "../../utils/getEvents";
+import { getGeoLocation } from "../../services/locationService";
 import EventCard from "../EventCard/EventCard";
 
 const Locator = ({ locations }) => {
-  // App context
   const { locationCtx, addLocation } = useContext(AppContext);
-
-  // Events state
   const [events, setEvents] = useState();
   const [loading, setLoading] = useState(true);
   const eventsFetchedRef = useRef(false);
-
-  // Loction state
   const [userLocation, setUserLocation] = useState();
   const [hasCity, setHasCity] = useState(false);
-  const dataLocationFetchedRef = useRef(false);
-  const geoFetchedRef = useRef(false);
-  const reverseGeoFetchedRef = useRef(false);
-  const setMatchesCity = (city) => {
-    if (city && matchesCity(city)) setHasCity(true);
-  };
 
   useEffect(() => {
-    const getGeoLocation = async () => {
-      let location;
-      // If we have a cookie, use it
-      if (hasUserLocation()) {
-        location = matchToLocation();
-        setUserLocation(location);
-        addLocation(location);
-        if (location.city) setMatchesCity(location.city);
-        return;
-      }
-
-      /**
-       * PRIMARY METHOD TO GET USERS LOCATION
-       * Browser GeoLocation
-       *
-       * Consider a hook: https://gist.github.com/whoisryosuke/e3ad7f43924dec60a12e247efe743e70
-       */
-      if (navigator.geolocation) {
-        location = navigator.geolocation.getCurrentPosition(
-          async (position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-
-            const getReverseGeo = async (latitude, longitude) => {
-              if (reverseGeoFetchedRef.current) return;
-              reverseGeoFetchedRef.current = true;
-
-              const url = urlBigData(latitude, longitude);
-              const locationResponse = await fetch(url);
-              const locationData = await locationResponse.json();
-              const { city, principalSubdivision: state } = locationData;
-
-              setMatchesCity(city);
-
-              const id = getLocationId(locations, city, state);
-              const locationObject = createLocationObject(city, state, id);
-
-              if (id) return locationObject;
-              return undefined;
-            };
-
-            let geoLocation = await getReverseGeo(latitude, longitude);
-
-            if (geoLocation) {
-              if (geoFetchedRef.current) return;
-              setUserLocation(geoLocation);
-              addLocation(geoLocation);
-
-              geoFetchedRef.current = true;
-            }
-          },
-          /**
-           * SECONDARY METHOD TO GET USERS LOCATION
-           * IF BROWSER LOCATION PERMISSION DENIED
-           */
-          (error) => {
-            if (error.PERMISSION_DENIED) {
-              const getUserLocation = async () => {
-                let location;
-                location = await UserLocationService();
-                setMatchesCity(location.city);
-                setUserLocation(location);
-                addLocation(location);
-              };
-
-              if (dataLocationFetchedRef.current) return;
-              dataLocationFetchedRef.current = true;
-              getUserLocation();
-            }
-          }
-        );
-      }
-    };
-    getGeoLocation();
+    getGeoLocation(locations, setUserLocation, addLocation, setHasCity);
   }, [locations, addLocation]);
 
   useEffect(() => {
---
+++ services/locationService.js
@@ -0,0 +1,83 @@
+import { urlBigData } from "../utils/utilities";
+import {
+  UserLocationService,
+  hasUserLocation,
+  matchToLocation,
+  matchesCity,
+  getLocationId,
+  createLocationObject,
+} from "../utils/getUserLocation.js";
+
+export const getGeoLocation = async (
+  locations,
+  setUserLocation,
+  addLocation,
+  setHasCity
+) => {
+  let location;
+  // If we have a cookie, use it
+  if (hasUserLocation()) {
+    location = matchToLocation();
+    setUserLocation(location);
+    addLocation(location);
+    if (location.city) setHasCity(matchesCity(location.city));
+    return;
+  }
+
+  if (navigator.geolocation) {
+    location = navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        await handleGeolocationSuccess(
+          latitude,
+          longitude,
+          locations,
+          setUserLocation,
+          addLocation,
+          setHasCity
+        );
+      },
+      (error) =>
+        handleGeolocationError(error, setUserLocation, addLocation, setHasCity)
+    );
+  }
+};
+
+const handleGeolocationSuccess = async (
+  latitude,
+  longitude,
+  locations,
+  setUserLocation,
+  addLocation,
+  setHasCity
+) => {
+  const url = urlBigData(latitude, longitude);
+  const locationResponse = await fetch(url);
+  const locationData = await locationResponse.json();
+  const { city, principalSubdivision: state } = locationData;
+
+  setHasCity(matchesCity(city));
+
+  const id = getLocationId(locations, city, state);
+  const locationObject = createLocationObject(city, state, id);
+
+  if (id) {
+    setUserLocation(locationObject);
+    addLocation(locationObject);
+  }
+};
+
+const handleGeolocationError = async (
+  error,
+  setUserLocation,
+  addLocation,
+  setHasCity
+) => {
+  if (error.PERMISSION_DENIED) {
+    const location = await UserLocationService();
+    setHasCity(matchesCity(location.city));
+    setUserLocation(location);
+    addLocation(location);
+  }
+};
