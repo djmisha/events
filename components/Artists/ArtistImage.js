ab3da4ab6ad3014ef17ea432aa9a62f6facec271
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2025-05-16 06:05:54 -0700

    Replace div with Image component in ArtistImage for better loading

--- components/Artists/ArtistImage.js
+++ components/Artists/ArtistImage.js
@@ -1,16 +1,25 @@
 import PropTypes from "prop-types";
+import Image from "next/image";
+import { useState } from "react";
 
 const ArtistImage = ({ id }) => {
+  const [imgError, setImgError] = useState(false);
   const url = id ? `/images/artists/${id}.jpg` : "/images/housemusic192.png";
+
+  if (imgError) return null;
 
   return (
     <div className="artist-fallback">
-      <div
+      <Image
         className="artist-image"
-        style={{
-          backgroundImage: `url('${url}')`,
-        }}
-      ></div>
+        src={url}
+        alt="Artist"
+        width={200} // Adjust as needed for your layout
+        height={200} // Adjust as needed for your layout
+        loading="lazy"
+        style={{ objectFit: "cover" }}
+        onError={() => setImgError(true)}
+      />
     </div>
   );
 };
